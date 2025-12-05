88cb22da3ab6f074e7e5508e79244c77f1df75ac
Sidhvi-1803 <sidhvi@efficientemenginerring.com>
2025-12-05 23:56:24 +0530

    Review Crud

---
+++ alembic/env.py
@@ -0,0 +1,123 @@
+import sys
+import os
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+
+# ===========================================
+# Load the FastAPI project to import settings
+# ===========================================
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+from app.config import settings
+from sqlmodel import SQLModel
+from app.models.book import Book
+from app.models.category import Category
+from app.models.user import User
+from app.models.review import Review  # when added
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+# ===========================================
+# Inject database URL from FastAPI settings.py
+# ===========================================
+config.set_main_option("sqlalchemy.url", settings.database_url)
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = SQLModel.metadata
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+
+EXCLUDE_TABLES = {
+    "auth_group",
+    "auth_permission",
+    "auth_group_permissions",
+    "auth_user",
+    "django_admin_log",
+    "django_content_type",
+    "django_session",
+    "django_migrations",
+    "customers",
+    "customers_groups",
+    "customers_user_permissions",
+    "token_blacklist_blacklistedtoken",
+    "token_blacklist_outstandingtoken"
+}
+
+def include_object(object, name, type_, reflected, compare_to):
+    # Ignore Django tables completely
+    if type_ == "table" and name in EXCLUDE_TABLES:
+        return False
+    return True
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+    url=url,
+    target_metadata=target_metadata,
+    compare_type=True,
+    include_object=include_object,
+    compare_server_default=True
+)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import create_db_and_tables
-from app.routes import auth, books_admin, categories_admin ,users , categories_public ,users , books_public , book_detail
+from app.routes import auth, books_admin, categories_admin ,users , categories_public ,users , books_public , book_detail , review
 import os
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
@@ -31,6 +31,7 @@ app.include_router(categories_admin.router,prefix="/admin/categories", tags=["Ad
 app.include_router(books_public.router, prefix="/books", tags=["Public Books"])
 app.include_router(categories_public.router, prefix="/categories", tags=["Public Categories"])
 app.include_router(book_detail.router, prefix="/book", tags=["Book Details"])
+app.include_router(review.router,prefix="/reviews",tags=["Reviews"])
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 UPLOAD_DIR = os.path.join(BASE_DIR, "uploads")
 
