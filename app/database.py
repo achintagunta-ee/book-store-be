74e6f6560756895dd6e856498991e4b64e2d3bc5
Sidhvi-1803 <sidhvi@efficientemenginerring.com>
2025-12-11 15:22:59 +0530

    updated main

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ engine = create_engine(settings.database_url, echo=True)
 
 def create_db_and_tables():
     from app.models import user, category, book , review ,cart , order
-    # SQLModel.metadata.create_all(engine)
+    SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
