5804e752dce0deacc5f1006bf28da1518be10dde
Sidhvi-1803 <sidhvi@efficientemenginerring.com>
2025-12-11 15:59:14 +0530

    resolve migration issue

--- app/models/wishlist.py
+++ app/models/wishlist.py
@@ -1,9 +1,10 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import datetime
 
 
 class Wishlist(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: int = Field(foreign_key="user.id")
     book_id: int = Field(foreign_key="book.id")
-    
+    created_at: datetime = Field(default_factory=datetime.utcnow)
